2686c7295781f725b4cdd1eac7442d05d9f01e5c
Mia Ikechukwu <mira.ikechukwu15@gmail.com>
2022-10-19 16:22:23 -0700

    Corrected some return values in _printf.c

--- _printf0.c
+++ _printf0.c
@@ -37,7 +37,7 @@ int _printf(const char *format, ...)
 	unsigned int i = 0, count = 0;
 
 	if (format == NULL)
-		return (1);
+		return (-1);
 	va_start(argp, format);
 	while (format[i])
 	{
@@ -57,7 +57,7 @@ int _printf(const char *format, ...)
 			continue;
 		}
 		if (!format[i + 1])
-			return (1);
+			return (-1);
 		_putchar(format[i]);
 		count++;
 		if (format[i + 1] == '%')
