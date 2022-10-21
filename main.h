77347d56e77695c552afc894b9312711e5702658
Betania <betania.berhanu@outlook.com>
2022-10-21 12:26:37 -0700

    6th

--- main.h
+++ main.h
@@ -7,6 +7,42 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/* FLAGS */
+#define F_MINUS 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
+/* SIZES */
+#define S_LONG 2
+#define S_SHORT 1
+
+/* Function to print non printable characters */
+int print_non_printable(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+
+/* Function to print memory address */
+int print_pointer(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+
+/* Functions to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+/*Function to print string in reverse*/
+int print_reverse(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+
+/*Function to print a string in rot 13*/
+int print_rot13string(va_list types, char buffer[],
+			int flags, int width, int precision, int size);
+
+
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list c);
