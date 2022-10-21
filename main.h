2bc0d6b5c63151db2f483d008cdb86a5d427bc7e
Betania <betania.berhanu@outlook.com>
2022-10-21 12:43:50 -0700

    6th

--- main.h
+++ main.h
@@ -7,28 +7,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/* Function to print non printable characters */
-int print_non_printable(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-
-/* Function to print memory address */
-int print_pointer(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-
-/* Functions to handle other specifiers */
-int get_flags(const char *format, int *i);
-int get_width(const char *format, int *i, va_list list);
-int get_precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int *i);
-/*Function to print string in reverse*/
-int print_reverse(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-
-/*Function to print a string in rot 13*/
-int print_rot13string(va_list types, char buffer[],
-			int flags, int width, int precision, int size);
-
-
 int _printf(const char *format, ...);
 int _putchar(char c);
 int print_char(va_list c);
