c2edd657fa39f584a94dc9d8874bdfe45f270b3b
Betania <betania.berhanu@outlook.com>
2022-10-21 14:35:07 -0700

    Fix Error

--- _printf0.c
+++ _printf0.c
@@ -1,79 +1,133 @@
 #include "main.h"
 
-/**
-* find_bet - function that finds a format for printf
-*
-* @format: format
-* Return: The number of characters printed
-*/
-int (*find_bet(const char *format))(va_list)
-{
-	unsigned int i = 0;
 
-	func_find find[] = {
-	{"c", print_char},
-	{"s", print_string},
-	{"d", print_decimal},
-	{"i", print_integer},
-	{"b", print_binary},
-	{"u", print_unsig},
-	{"o", print_octal},
-	{"x", print_x},
-	{"X", print_X},
-	{"r", print_reverse},
-	{"R", print_rot13},
-	{NULL, NULL}
-};
-	while (find[i].sf)
-	{
-	if (find[i].sf[0] == (*format))
-		return (find[i].f);
-	i++;
-	}
-	return (NULL);
-}
+
+void print_buffer(char buffer[], int *buff_ind);
+
+
 
 /**
-* _printf - function that prints to the stdout
-* @format: format (char , string)
-* Return: the number of chars printed (excluding the null byte)
-*/
+
+ * _printf - Printf function
+
+ * @format: format.
+
+ * Return: Printed chars.
+
+ */
+
 int _printf(const char *format, ...)
-{
-	va_list argp;
-	int (*f)(va_list);
 
-	unsigned int i = 0, count = 0;
+{
+
+	int i, printed = 0, printed_chars = 0;
+
+	int flags, width, precision, size, buff_ind = 0;
+
+	va_list list;
+
+	char buffer[BUFF_SIZE];
+
+
 
 	if (format == NULL)
+
 		return (-1);
-	va_start(argp, format);
-	while (format[i])
+
+
+
+	va_start(list, format);
+
+
+
+	for (i = 0; format && format[i] != '\0'; i++)
+
 	{
-		while (format[i] != '%' && format[i])
+
+		if (format[i] != '%')
+
 		{
-			_putchar(format[i]);
-			count++;
-			i++;
+
+			buffer[buff_ind++] = format[i];
+
+			if (buff_ind == BUFF_SIZE)
+
+				print_buffer(buffer, &buff_ind);
+
+			/* write(1, &format[i], 1);*/
+
+			printed_chars++;
+
 		}
-		if (format[i] == '\0')
-			return (count);
-		f = find_bet(&format[i + 1]);
-		if (f != NULL)
-		{
-			count += f(argp);
-			i += 2;
-			continue;
-		}
-		if (!format[i + 1])
-			return (-1);
-		_putchar(format[i]);
-		count++;
-		if (format[i + 1] == '%')
-			i += 2;
+
 		else
-			i++;
+
+		{
+
+			print_buffer(buffer, &buff_ind);
+
+			flags = get_flags(format, &i);
+
+			width = get_width(format, &i, list);
+
+			precision = get_precision(format, &i, list);
+
+			size = get_size(format, &i);
+
+			++i;
+
+			printed = handle_print(format, &i, list, buffer,
+
+				flags, width, precision, size);
+
+			if (printed == -1)
+
+				return (-1);
+
+			printed_chars += printed;
+
+		}
+
 	}
-	va_end(argp);
-	return (count);
+
+
+
+	print_buffer(buffer, &buff_ind);
+
+
+
+	va_end(list);
+
+
+
+	return (printed_chars);
+
 }
+
+
+
+/**
+
+ * print_buffer - Prints the contents of the buffer if it exist
+
+ * @buffer: Array of chars
+
+ * @buff_ind: Index at which to add next char, represents the length.
+
+ */
+
+void print_buffer(char buffer[], int *buff_ind)
+
+{
+
+	if (*buff_ind > 0)
+
+		write(1, &buffer[0], *buff_ind);
+
+
+
+	*buff_ind = 0;
+
+}
+
+
